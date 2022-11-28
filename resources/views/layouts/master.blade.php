ac01a6dca794396c24bf87279a7493ed556c8f97
DESKTOP-1J6DIPV\furka <furkan384@hotmail.com>
2022-11-28 22:18:49 +0300

    css update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/x-icon/01.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/x-icon/01.png?v=1.1.0">
 
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/css/all.min.css">
-    <link rel="stylesheet" href="/assets/css/lightcase.css">
-    <link rel="stylesheet" href="/assets/css/swiper.min.css">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css?v=1.1.0">
+    <link rel="stylesheet" href="/assets/css/all.min.css?v=1.1.0">
+    <link rel="stylesheet" href="/assets/css/lightcase.css?v=1.1.0">
+    <link rel="stylesheet" href="/assets/css/swiper.min.css?v=1.1.0">
+    <link rel="stylesheet" href="/assets/css/style.css?v=1.1.0">
     <style type="text/css">
         @font-face {
             font-family: TrajanSans;
