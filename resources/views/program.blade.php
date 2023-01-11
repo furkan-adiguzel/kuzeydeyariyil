f79f9dec5a806d51daa3c957e2cde76f84b21184
DESKTOP-1J6DIPV\furka <furkan384@hotmail.com>
2023-01-11 03:43:42 +0300

    program update

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -13,172 +13,126 @@
     </section>
     <!-- Page Header Section Ending Here -->
 
-    <h3 class="coming-soon-title mb-4" style="width: 80vw;margin: auto;text-align: center; margin-top:2rem;">Çok Yakında Program Bilgilerini Paylaşacağız.</h3>
+{{--    <h3 class="coming-soon-title mb-4" style="width: 80vw;margin: auto;text-align: center; margin-top:2rem;">Çok Yakında Program Bilgilerini Paylaşacağız.</h3>--}}
 
-    {{--    <div class="container padding-tb-sm">--}}
-{{--        <div class="row justify-content-center">--}}
-{{--            <div class="col-lg-9">--}}
+        <div class="container padding-tb-sm">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
 
-{{--                <table>--}}
-{{--                    <thead>--}}
-{{--                    <tr>--}}
-{{--                        <th width="200">Cuma Programı</th>--}}
-{{--                        <th>Etkinlik</th>--}}
-{{--                    </tr>--}}
-{{--                    </thead>--}}
-{{--                    <tbody>--}}
-{{--                    <tr>--}}
-{{--                        <td>13:00 - 13:00</td>--}}
-{{--                        <td>Otele Giriş</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>13:00 - 18:00</td>--}}
-{{--                        <td>Etkinlik ve Workshop</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>17:45 - 18:00</td>--}}
-{{--                        <td>Hoşgeldiniz Sunumu / Program Tanıtımı</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>18:00 - 19:30</td>--}}
-{{--                        <td>Etkinlik (Oyun)</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>19:30 - 21:00</td>--}}
-{{--                        <td>AKŞAM YEMEĞİ</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>20:00 - 22:00</td>--}}
-{{--                        <td>GELECEK DÖNEM BAŞKANLARI TANIŞMA TOPLANTISI / BAYRAK TÖRENİ PROVASI</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>22:00 - 00:00</td>--}}
-{{--                        <td>KONSEPT PARTİ</td>--}}
-{{--                    </tr>--}}
-{{--                    </tbody>--}}
-{{--                </table>--}}
-{{--                <br>--}}
-{{--                <table>--}}
-{{--                    <thead>--}}
-{{--                    <tr>--}}
-{{--                        <th width="200">Cumartesi Programı</th>--}}
-{{--                        <th>Etkinlik</th>--}}
-{{--                    </tr>--}}
-{{--                    </thead>--}}
-{{--                    <tbody>--}}
-{{--                    <tr>--}}
-{{--                        <td>07:00 - 08:30</td>--}}
-{{--                        <td>KAHVALTI</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>09:00 - 09:55</td>--}}
-{{--                        <td>AÇILIŞ OTURUMU / BAYRAK TÖRENİ</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>09:55 - 10:00</td>--}}
-{{--                        <td>SAYGI DURUŞU / İSTAKLAL MARŞI VE GENÇLİĞE HİTABE</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>11:00 - 10:10</td>--}}
-{{--                        <td>ASAMBLE KOMİTESİ BAŞKANI / CEREN ARSAL</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:10 - 10:20</td>--}}
-{{--                        <td>İZMİR AGORA ROTARACT BAŞKANI / ÖZENÇ AKOVA </td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:20 - 10:25</td>--}}
-{{--                        <td>UR. 2440. DÖNEM ROTARY FEDERASYONU ROTARACTLARDAN SORUMLU GUVERNÖR YARDIMCISI / RTN. SEDAT HACIARİFOĞLU </td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:25 - 10:35</td>--}}
-{{--                        <td>UR. 2440. DÖNEM ROTARY FEDERASYONU DÖNEM GUVERNÖRÜ KONUŞMA / RTN. NEDİM ATİLLA</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:35 - 10:45</td>--}}
-{{--                        <td>UR. 2440. BÖLGE ROTARACT TEMSİLCİSİ / MELİS UZŞEN</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:45 - 10:50</td>--}}
-{{--                        <td>UR. 2440. ROTARY FEDERASYONU 2016- 2017 DÖNEMİ GUVERNÖRÜ - DÖNEM GELECEK KUŞAKLAR KOMİTESİ BAŞKANI KONUŞMA / RTN. İSMAİL RODOPLU</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:50 - 11:00</td>--}}
-{{--                        <td>UR. 2440. DÖNEM ROTARY FEDERASYONU GELECEK DÖNEM GUVERNÖR ADAYI KONUŞMA / RTN. SERDAR DURUSÜT</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>11:00 - 12:00</td>--}}
-{{--                        <td>UR. 2440. BÖLGE ROTARACT TEMSİLCİSİ / EGE YİĞİT ECE / UR. 2440. BÖLGE ÇALIŞMA GRUBU'NUN TANITIMI</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>12:00 - 12:30</td>--}}
-{{--                        <td>KAHVE MOLASI</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>12:30 - 13:00</td>--}}
-{{--                        <td>TOPLU FOTOĞRAF ÇEKİMİ</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>13:00 - 14:30</td>--}}
-{{--                        <td>ÖĞLE YEMEĞİ</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>14:30 - 16:30</td>--}}
-{{--                        <td>EĞİTİMLER</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>16:00 - 17:30</td>--}}
-{{--                        <td>ETKİNLİK</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>17:30 - 18:00</td>--}}
-{{--                        <td>KAPANIŞ OTURUMU</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>18:00 - 19:00</td>--}}
-{{--                        <td>SERBEST ZAMAN</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>19:00 - 20:00</td>--}}
-{{--                        <td>GALA ÖNCESİ KOKTEYL VE FOTOGRAF ÇEKİMİ</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>20:00 - 00:00</td>--}}
-{{--                        <td>GALA YEMEĞİ</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>00:00 - 02:00</td>--}}
-{{--                        <td>AFTER PARTİ</td>--}}
-{{--                    </tr>--}}
-{{--                    </tbody>--}}
-{{--                </table>--}}
-{{--                <br>--}}
-{{--                <table>--}}
-{{--                    <thead>--}}
-{{--                    <tr>--}}
-{{--                        <th width="200">Pazar Programı</th>--}}
-{{--                        <th>Etkinlik</th>--}}
-{{--                    </tr>--}}
-{{--                    </thead>--}}
-{{--                    <tbody>--}}
-{{--                    <tr>--}}
-{{--                        <td>08:00 - 10:00</td>--}}
-{{--                        <td>KAHVALTI</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>10:00 - 12:00</td>--}}
-{{--                        <td>Fotograf Seçimi / Kahve Molası</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>12:00 - 13:00</td>--}}
-{{--                        <td>Otelden Ayrılış</td>--}}
-{{--                    </tr>--}}
-{{--                    </tbody>--}}
-{{--                </table>--}}
-{{--                <br>--}}
-{{--            </div>--}}
-{{--       </div>--}}
-{{--    </div>--}}
+                <table>
+                    <thead>
+                    <tr>
+                        <th width="200">13 Ocak Cuma</th>
+                        <th>Etkinlik</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>14:00 - 18:00</td>
+                        <td>Otele Giriş - Serbest Zaman</td>
+                    </tr>
+                    <tr>
+                        <td>18.30 - 20.00</td>
+                        <td>Otelde Akşam Yemeği</td>
+                    </tr>
+                    <tr>
+                        <td>20.00 - 00.00</td>
+                        <td>Eğlence - Ayvalık Merkez (Ekstra ücretlendirme içerir.)</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <br>
+                <table>
+                    <thead>
+                    <tr>
+                        <th width="200">14 Ocak Cumartesi</th>
+                        <th>Etkinlik</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>09:00 - 11:00</td>
+                        <td>Sunumlar 1. Oturum (Tüm katılımcılara açıktır.)</td>
+                    </tr>
+                    <tr>
+                        <td>11:00 - 11:15</td>
+                        <td>Coffee Break</td>
+                    </tr>
+                    <tr>
+                        <td>11:15 - 12:45</td>
+                        <td>Sunumlar 2. oturum (Tüm katılımcılara açıktır.)</td>
+                    </tr>
+                    <tr>
+                        <td>12:45 - 13:30</td>
+                        <td>Otelde Öğle Yemeği</td>
+                    </tr>
+                    <tr>
+                        <td>13.30 - 18.00</td>
+                        <td>Cunda Turu - Tekne Partisi (YK dışındaki tüm rotaractörlere açıktır.)</td>
+                    </tr>
+                    <tr>
+                        <td>13:30 - 14:30</td>
+                        <td>Başkanlar - İlk oturum (YK üyelerinin katılımına açıktır.) </td>
+                    </tr>
+                    <tr>
+                        <td>14:30 - 14:45</td>
+                        <td>Coffee Break</td>
+                    </tr>
+                    <tr>
+                        <td>14:45 - 16:00</td>
+                        <td>Başkanlar - İkinci oturum (YK üyelerinin katılımına açıktır.) </td>
+                    </tr>
+                    <tr>
+                        <td>16:00 - 16:15</td>
+                        <td>Coffee Break</td>
+                    </tr>
+                    <tr>
+                        <td>16:15 - 17:00</td>
+                        <td>Açık Oturum (Tüm katılımcılara açıktır.)</td>
+                    </tr>
+                    <tr>
+                        <td>17:00 - 18:00</td>
+                        <td>Asbaşkanlar Oturumu</td>
+                    </tr>
+                    <tr>
+                        <td>18.00 - 19.30</td>
+                        <td>Serbest Zaman</td>
+                    </tr>
+                    <tr>
+                        <td>19.30 - 23.30</td>
+                        <td>Gala Yemeği</td>
+                    </tr>
+                    <tr>
+                        <td>23.30 - 01.00</td>
+                        <td>After Party</td>
+                    </tr>
+
+
+                    </tbody>
+                </table>
+                <br>
+                <table>
+                    <thead>
+                    <tr>
+                        <th width="200">15 Ocak Pazar</th>
+                        <th>Etkinlik</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>07.00 - 10.00</td>
+                        <td>Otelde Kahvaltı</td>
+                    </tr>
+                    <tr>
+                        <td>10.00 - 12.00</td>
+                        <td>Serbest Zaman - Otelden Ayrılış</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <br>
+            </div>
+       </div>
+    </div>
 
 
 @endsection
